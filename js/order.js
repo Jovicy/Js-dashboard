c2e159a87034b21f5f9ef794571aad7fecef48cd
unknown <johnvictordml@gmail.com>
2024-03-22 04:45:29 +0100

    first commit

---
+++ js/order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Lavender KF102 Drone',
+        productNumber: '85631',
+        productStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
